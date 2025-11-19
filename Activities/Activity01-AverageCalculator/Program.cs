0a80845ccc67b7ac85a66542a92482cbfdd1b0a5
jgoode7 <joegoode.07@gmail.com>
2025-11-19 11:25:15 -0800

    Complete calculator program
    Tested and working

--- Activities/Activity01-AverageCalculator/Program.cs
+++ Activities/Activity01-AverageCalculator/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("=== Average Calculator ===");
+
+// Get Three Numbers from the User
+Console.Write("Enter the first number: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the third number: ");
+double num3 = Convert.ToDouble(Console.ReadLine());
+
+// Calculate the Average
+double average = (num1 + num2 + num3) / 3;
+
+// Display the Result
+Console.WriteLine($"The average is: {average}");
