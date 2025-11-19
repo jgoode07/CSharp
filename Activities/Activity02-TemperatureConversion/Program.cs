8989b59aa979e77fdc95cbff7c12ec3b7b6a1367
jgoode7 <joegoode.07@gmail.com>
2025-11-19 11:47:56 -0800

    Complete temp conversion program
    Tested and working

--- Activities/Activity02-TemperatureConversion/Program.cs
+++ Activities/Activity02-TemperatureConversion/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("=== Fahrenheit to Celsius Converter ===");
+
+// Get temperature from user
+Console.Write("Enter the temperature in Fahrenheit: ");
+double fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+// Convert to Celsius
+double celsius = (fahrenheit - 32) * 5 / 9;
+
+// Display the result
+Console.WriteLine($"Temperature in Celsius: {celsius}");
