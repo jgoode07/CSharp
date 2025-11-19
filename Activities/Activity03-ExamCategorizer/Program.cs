588631e36902541d7a0c312836ecf9f9af294841
jgoode7 <joegoode.07@gmail.com>
2025-11-19 12:42:51 -0800

    Add looping and validation to exam categorizer

--- Activities/Activity03-ExamCategorizer/Program.cs
+++ Activities/Activity03-ExamCategorizer/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool keepGoing = true;
+
+while (keepGoing)
+{
+    Console.WriteLine("=== Exam Score Categorizer ===");
+
+    // Validate User Input (Must be between 0 and 100)
+    double score;
+    while (true)
+    {
+        Console.Write("Enter a score (0-100): ");
+        string input = Console.ReadLine();
+
+        if (double.TryParse(input, out score) && score >= 0 && score <= 100)
+        {
+            break; // Valid Input
+        }
+        else
+        {
+            Console.WriteLine("Invalid score. Please enter a number between 0 and 100.");
+        }
+    }
+}
