4db49b5cf0b0d6a50f3752c2a03dfc46630ed9ab
jgoode7 <joegoode.07@gmail.com>
2025-11-20 11:34:42 -0800

    Add first 4 operations
    Everything except power to "^"

--- Assignments/Assignment01-Calculator/Program.cs
+++ Assignments/Assignment01-Calculator/Program.cs
@@ -60,3 +60,25 @@ else
 }
 
 Console.WriteLine($"{first} {symbol} {second} = {result}");
+
+// Functions for Each Operation
+
+static double Add(double a, double b)
+{
+    return a + b;
+}
+
+static double Subtract(double a, double b)
+{
+    return a - b;
+}
+
+static double Multiply(double a, double b)
+{
+    return a * b;
+}
+
+static double Divide(double a, double b)
+{
+    return a / b;
+}
