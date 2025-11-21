7c5233599adc8ce5fb5281a70b613024ed095eb7
jgoode7 <joegoode.07@gmail.com>
2025-11-21 08:34:22 -0800

    Add user prompts for first and last name

--- Assignments/Assignment02-EmailGenerator/Program.cs
+++ Assignments/Assignment02-EmailGenerator/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter your first name: ");
+string firstName = Console.ReadLine();
+
+Console.Write("Enter your last name: ");
+string lastName = Console.ReadLine();
