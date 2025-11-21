665c5c315e54f0cccd5f2412578f2bf19b89a660
jgoode7 <joegoode.07@gmail.com>
2025-11-21 08:52:33 -0800

    Add birthYear input with exception handling

--- Assignments/Assignment02-EmailGenerator/Program.cs
+++ Assignments/Assignment02-EmailGenerator/Program.cs
@@ -3,3 +3,18 @@ string firstName = Console.ReadLine();
 
 Console.Write("Enter your last name: ");
 string lastName = Console.ReadLine();
+
+int birthYear;
+
+// Try/catch to make sure the year of birth is a valid number.
+try
+{
+    Console.Write("Enter your year of birth: ");
+    birthYear = Convert.ToInt32(Console.ReadLine());
+}
+catch
+{
+    Console.WriteLine("Invalid year. Please enter a valid number next time.");
+    Environment.Exit(0);
+    return;
+}
