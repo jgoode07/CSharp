c4b02331c9fdd06c1548cad44a1f0b5cc20fbc50
jgoode7 <joegoode.07@gmail.com>
2025-11-26 10:40:54 -0800

    Add Animal base class with name, age, and virtual MakeSound()

---
+++ Activities/Activity04-ZooAnimals/Animal.cs
@@ -0,0 +1,16 @@
+public class Animal
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Animal(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public virtual string MakeSound()
+    {
+        return "Generic animal sound";
+    }
+}
