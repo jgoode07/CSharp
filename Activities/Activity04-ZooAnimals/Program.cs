ef3b66ed64979944f1d26d04999f5687f3d6488c
jgoode7 <joegoode.07@gmail.com>
2025-11-26 19:46:16 -0800

    Add Zoo setup to Program.cs and test run

--- Activities/Activity04-ZooAnimals/Program.cs
+++ Activities/Activity04-ZooAnimals/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿// Create a New Zoo
+Zoo myZoo = new Zoo();
+
+// Create Animals
+Lion leo = new Lion("Leo", 5);
+Elephant ella = new Elephant("Ella", 12);
+Penguin penny = new Penguin("Penny", 3);
+
+// Add Animals to the Zoo
+myZoo.AddAnimal(leo);
+myZoo.AddAnimal(ella);
+myZoo.AddAnimal(penny);
+
+// Show Info about all Animals
+myZoo.DisplayAnimals();
+
+Console.WriteLine();
+
+// Make all Animals Perform their Sounds
+myZoo.MakeAllSounds();
