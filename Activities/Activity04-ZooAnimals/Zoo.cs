20a86b956b185884b82e8c0aceba15fddd606d17
jgoode7 <joegoode.07@gmail.com>
2025-11-26 19:08:44 -0800

    Add display, sound methods

--- Activities/Activity04-ZooAnimals/Zoo.cs
+++ Activities/Activity04-ZooAnimals/Zoo.cs
@@ -10,4 +10,22 @@ public class Zoo
     {
         animals.Add(animal);
     }
+
+    // Show Info for all Animals
+    public void DisplayAnimals()
+    {
+        foreach (var animal in animals)
+        {
+            Console.WriteLine($"{animal.Name}, Age {animal.Age}, Sound: {animal.MakeSound()}");
+        }
+    }
+
+    // // Have Each Animal Make its Sound
+    public void MakeAllSounds()
+    {
+        foreach (var animal in animals)
+        {
+            Console.WriteLine(animal.MakeSound());
+        }
+    }
 }
