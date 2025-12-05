7267fcbe40aceef8c4726bdd22503a58598059a3
jgoode7 <joegoode.07@gmail.com>
2025-12-04 17:57:39 -0800

    Add empty input validation

--- Activities/Activity06-StringManipulationApp/Program.cs
+++ Activities/Activity06-StringManipulationApp/Program.cs
@@ -1,2 +1,8 @@
 ﻿Console.Write("Enter a sentence or phrase: ");
 string? userInput = Console.ReadLine();
+
+if (string.IsNullOrEmpty(userInput))
+{
+    Console.WriteLine("You didn't enter any text. Please run the program again.");
+    return;
+}
