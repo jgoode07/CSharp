8e56d532f5113edabde3226010ecec8e28ef64d3
jgoode7 <joegoode.07@gmail.com>
2025-12-05 11:12:39 -0800

    Add random integer list

--- Activities/Activity08-CollectionManipulation/Program.cs
+++ Activities/Activity08-CollectionManipulation/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+List<int> numbers = new List<int>();
+Random random = new Random();
+
+// Create 10 random numbers between 1 and 50
+for (int i = 0; i < 10; i++)
+{
+    numbers.Add(random.Next(1, 51));
+}
+
+// Display the original list
+Console.WriteLine("Original List of Numbers:");
+foreach (int num in numbers)
+{
+    Console.Write(num + " ");
+}
+
+Console.WriteLine();
