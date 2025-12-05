08dfa39c92a029df35bf117c0ea4208d2950b544
jgoode7 <joegoode.07@gmail.com>
2025-12-04 17:42:02 -0800

    Add user input

--- Activities/Activity06-StringManipulationApp/Program.cs
+++ Activities/Activity06-StringManipulationApp/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter a sentence or phrase: ");
+string? userInput = Console.ReadLine();
