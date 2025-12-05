4570ee43034942eddfcb1a8315bc780f84b30c14
jgoode7 <joegoode.07@gmail.com>
2025-12-04 19:49:41 -0800

    Add user input for date

--- Activities/Activity07-DateTimeApp/Program.cs
+++ Activities/Activity07-DateTimeApp/Program.cs
@@ -17,5 +17,11 @@ class Program
         Console.WriteLine("Date After Adding 5 Days:");
         Console.WriteLine(futureDate.ToString("MMMM dd, yyyy"));
         Console.WriteLine();
+
+        // Ask the user for a date
+        Console.Write("Enter a date (yyyy-mm-dd): ");
+        string userInput = Console.ReadLine();
+
+        DateTime userDate = DateTime.Parse(userInput);
     }
 }
