5429c347f77bc7758f45121b34e5c23f8901481f
jgoode7 <joegoode.07@gmail.com>
2025-12-04 19:42:32 -0800

    Display current date, add future date

--- Activities/Activity07-DateTimeApp/Program.cs
+++ Activities/Activity07-DateTimeApp/Program.cs
@@ -6,5 +6,16 @@ class Program
     {
         // Get the current date and time
         DateTime currentDateTime = DateTime.Now;
+
+        Console.WriteLine("Current Date and Time:");
+        Console.WriteLine(currentDateTime.ToString("MMMM dd, yyyy - hh:mm tt"));
+        Console.WriteLine();
+
+        // Add 5 days to the current date
+        DateTime futureDate = currentDateTime.AddDays(5);
+
+        Console.WriteLine("Date After Adding 5 Days:");
+        Console.WriteLine(futureDate.ToString("MMMM dd, yyyy"));
+        Console.WriteLine();
     }
 }
