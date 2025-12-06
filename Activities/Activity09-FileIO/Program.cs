8ae3e1140268c8ef0674e8bd91eeca96117673bd
jgoode7 <joegoode.07@gmail.com>
2025-12-05 17:47:30 -0800

    Create text file with sample content

--- Activities/Activity09-FileIO/Program.cs
+++ Activities/Activity09-FileIO/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+string filePath = "activity9.txt";
+
+// Only create the file if it doesn't already exist
+if (!File.Exists(filePath))
+{
+    string sampleContent =
+        "Buy the ticket, take the ride.\n" +
+        "- Hunter S. Thompson\n\n" +
+        "Not all those who wander are lost.\n" +
+        "- J.R.R. Tolkien\n";
+
+    File.WriteAllText(filePath, sampleContent);
+
+    Console.WriteLine("Sample file created with initial content.");
+}
+else
+{
+    Console.WriteLine("File already exists. Using existing file.");
+}
