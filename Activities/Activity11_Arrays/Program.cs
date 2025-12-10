39bf9b46faff0503b2fce9cf5721aac9345d9193
jgoode7 <joegoode.07@gmail.com>
2025-12-10 15:58:38 -0800

    Add array with random numbers`

--- Activities/Activity11_Arrays/Program.cs
+++ Activities/Activity11_Arrays/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+// Create an array of integers
+int[] numbers = new int[10];
+
+// Fill the array with random numbers (1–100)
+Random rand = new Random();
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = rand.Next(1, 101);
+}
