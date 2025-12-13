94cc319df26834bec68106383ef77d52b262f191
jgoode7 <joegoode.07@gmail.com>
2025-12-12 20:01:56 -0800

    Add XML serialization for List<Shape>

--- Assignments/Assignment05-XmlShapes/Program.cs
+++ Assignments/Assignment05-XmlShapes/Program.cs
@@ -9,3 +9,17 @@ var listOfShapes = new List<Shape>
     new Circle { Colour = "Purple", Radius = 12.3 },
     new Rectangle { Colour = "Blue", Height = 45.0, Width = 18.0 }
 };
+
+// File path for saving the XML output
+string xmlPath = "shapes.xml";
+
+// Create the serializer for a List<Shape>
+var serializer = new XmlSerializer(typeof(List<Shape>));
+
+// Write the XML file
+using (var file = File.Create(xmlPath))
+{
+    serializer.Serialize(file, listOfShapes);
+}
+
+Console.WriteLine("Shapes saved to XML.");
