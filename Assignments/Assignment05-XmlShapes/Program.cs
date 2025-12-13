89a42cb4f2d80ca8dd02793b48905ac4080bfdb7
jgoode7 <joegoode.07@gmail.com>
2025-12-12 19:22:00 -0800

    Add initial list of shapes in Program.cs

--- Assignments/Assignment05-XmlShapes/Program.cs
+++ Assignments/Assignment05-XmlShapes/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Xml.Serialization;
+
+// Create a list of Shapes to serialize
+var listOfShapes = new List<Shape>
+{
+    new Circle { Colour = "Red", Radius = 2.5 },
+    new Rectangle { Colour = "Blue", Height = 20.0, Width = 10.0 },
+    new Circle { Colour = "Green", Radius = 8 },
+    new Circle { Colour = "Purple", Radius = 12.3 },
+    new Rectangle { Colour = "Blue", Height = 45.0, Width = 18.0 }
+};
