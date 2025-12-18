95194708b18f0eb1f360872ca48096fa66f1b320
jgoode7 <joegoode.07@gmail.com>
2025-12-18 15:20:23 -0800

    Add Customer model

---
+++ Activities/Activity13-EFCoreNorthwind/Activity13-EFCoreNorthwind/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Activity13_EFCoreNorthwind.Models
+{
+    public class Customer
+    {
+        public string CustomerId { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+    }
+}
