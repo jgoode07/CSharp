89227706cf3a0476faa3df53f84d05e524333b7d
jgoode7 <joegoode.07@gmail.com>
2025-12-18 13:29:03 -0800

    Add Northwind DbContext

---
+++ Activities/Activity13-EFCoreNorthwind/Activity13-EFCoreNorthwind/Data/NorthwindContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Activity13_EFCoreNorthwind.Data
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(
+                "Server=.;Database=Northwind;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+    }
+}
