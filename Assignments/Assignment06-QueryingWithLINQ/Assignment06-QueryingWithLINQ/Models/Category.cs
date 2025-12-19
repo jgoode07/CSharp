71a696f3b41b9230fd7b7d1445525e1309cca11d
jgoode7 <joegoode.07@gmail.com>
2025-12-19 11:56:04 -0800

    Add Category model for Northwind

---
+++ Assignments/Assignment06-QueryingWithLINQ/Assignment06-QueryingWithLINQ/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Activity13_EFCoreNorthwind.Models
+{
+    // Represents a category from the Northwind database
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+    }
+}
