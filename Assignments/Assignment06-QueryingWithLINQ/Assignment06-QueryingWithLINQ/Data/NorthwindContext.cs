a6f379c7358263ee2701ab4ba20e9e14ce303742
jgoode7 <joegoode.07@gmail.com>
2025-12-19 12:36:04 -0800

    Add Northwind DbContext

---
+++ Assignments/Assignment06-QueryingWithLINQ/Assignment06-QueryingWithLINQ/Data/NorthwindContext.cs
@@ -0,0 +1,19 @@
+﻿using Activity13_EFCoreNorthwind.Models;
+using Assignment06_LinqNorthwind.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment06_LinqNorthwind.Data
+{
+    // DbContext used to access the Northwind database
+    public class NorthwindContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(
+                "Server=.;Database=Northwind;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+    }
+}
