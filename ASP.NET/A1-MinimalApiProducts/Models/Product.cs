ddb73101f5aef243a2a0e3609e0721ea8d885d45
jgoode7 <joegoode.07@gmail.com>
2026-01-11 13:59:56 -0800

    Add Product model for minimal API

---
+++ ASP.NET/A1-MinimalApiProducts/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace A1_MinimalApiProducts.Models
+{
+    // Simple product model used by the minimal API
+    public class Product
+    {
+        // Unique ID for each product
+        public int ProductId { get; set; }
+
+        // Name shown to the user
+        public string Name { get; set; } = string.Empty;
+
+        // Short description of the product
+        public string Description { get; set; } = string.Empty;
+
+        // Price of the product
+        public decimal Price { get; set; }
+    }
+}
