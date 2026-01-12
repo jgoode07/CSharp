fddd1deae9c3d50a2079b1ee97f53c403b1c8a51
jgoode7 <joegoode.07@gmail.com>
2026-01-11 18:28:33 -0800

    Map product routes in Program.cs

--- ASP.NET/A1-MinimalApiProducts/Program.cs
+++ ASP.NET/A1-MinimalApiProducts/Program.cs
@@ -1,41 +1,21 @@
+using A1_MinimalApiProducts.Endpoints;
+using A1_MinimalApiProducts.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// Product endpoints
+app.MapPost("/products", (Product product) => ProductEndpoint.CreateProduct(product));
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapGet("/products", () => ProductEndpoint.GetAllProducts());
+
+app.MapGet("/products/{id:int}", (int id) => ProductEndpoint.GetProductById(id));
+
+app.MapDelete("/products/{id:int}", (int id) => ProductEndpoint.DeleteProduct(id));
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
