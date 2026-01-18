b83e7b678c1eb9795de742bc780dbb9c46767c51
jgoode7 <joegoode.07@gmail.com>
2026-01-18 12:28:17 -0800

    Add AppDbContext

---
+++ ASP.NET/A2-AccountManagement/Data/AppDbConext.cs
@@ -0,0 +1,14 @@
+﻿using A2_AccountManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace A2_AccountManagement.Data;
+
+public class AppDbContext : DbContext
+{
+    // Constructor that receives options from Program.cs
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Account> Accounts { get; set; } = null!;
+}
