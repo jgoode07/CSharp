23f767b314615f5bb28fcc1ca8fbc78e8d944c81
jgoode7 <joegoode.07@gmail.com>
2026-01-18 15:11:55 -0800

    Add account endpoints

--- ASP.NET/A2-AccountManagement/Program.cs
+++ ASP.NET/A2-AccountManagement/Program.cs
@@ -1,4 +1,5 @@
 using A2_AccountManagement.Data;
+using A2_AccountManagement.Models;
 using A2_AccountManagement.Services;
 using Microsoft.EntityFrameworkCore;
 
@@ -22,5 +23,34 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Register a new account
+app.MapPost("/accounts", (Account account, AccountService service) =>
+{
+    var created = service.Create(account);
+    return Results.Created($"/accounts/{created.Id}", created);
+});
+
+// Get account by ID
+app.MapGet("/accounts/{id:int}", (int id, AccountService service) =>
+{
+    var account = service.GetById(id);
+    return account == null ? Results.NotFound() : Results.Ok(account);
+});
+
+// Get account by email (query string)
+app.MapGet("/accounts/by-email", (string email, AccountService service) =>
+{
+    var account = service.GetByEmail(email);
+    return account == null ? Results.NotFound() : Results.Ok(account);
+});
+
+// Update account by ID
+app.MapPut("/accounts/{id:int}", (int id, Account account, AccountService service) =>
+{
+    account.Id = id;
+    var updated = service.Update(account);
+    return updated ? Results.NoContent() : Results.NotFound();
+});
 
 app.Run();
