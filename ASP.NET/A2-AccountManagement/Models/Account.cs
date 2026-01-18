1e7d7599974135c2a8f55578c0580285ebae0b42
jgoode7 <joegoode.07@gmail.com>
2026-01-18 12:11:06 -0800

    Add account model

---
+++ ASP.NET/A2-AccountManagement/Models/Account.cs
@@ -0,0 +1,12 @@
+﻿namespace A2_AccountManagement.Models;
+
+public class Account
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = "";
+
+    public string LastName { get; set; } = "";
+
+    public string Email { get; set; } = "";
+}
