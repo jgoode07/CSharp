64f1afdf823ce3e0257c074b2fb8f7c8f5ebdb67
jgoode7 <joegoode.07@gmail.com>
2026-01-24 15:19:30 -0800

    Add Swagger and initial API setup

---
+++ ASP.NET/A3-ProductsApi/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using A3_ProductsApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace A3_ProductsApi.Data;
+
+public class AppDbContext : DbContext
+{
+    // EF Core options are passed in from Program.cs (dependency injection)
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Price stored as decimal(18,2)
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Price)
+            .HasPrecision(18, 2);
+    }
+}
