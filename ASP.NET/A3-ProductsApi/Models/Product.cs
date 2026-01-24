fb76f8a0e2038f11aa6c9de7aa13ddedf38b7963
jgoode7 <joegoode.07@gmail.com>
2026-01-24 14:12:14 -0800

    Add Product model, remove template weather files

---
+++ ASP.NET/A3-ProductsApi/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace A3_ProductsApi.Models;
+
+public class Product
+{
+    // Primary key
+    public int Id { get; set; }
+
+    // Basic product info
+    public string Name { get; set; } = "";
+
+    public string Description { get; set; } = "";
+
+    // Using decimal for money values
+    public decimal Price { get; set; }
+}
